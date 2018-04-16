cb751f2cccdb4313413138304d60121e2ce946ed
Szász Tibor <tszasz@gmail.com>
2018-04-16 16:21:05 +0200

    refact(js): remove intersection observer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// @todo Load polyfill for IntersectionObserver
-
 class UI {
 	constructor() {
 		this.modal = null;
@@ -9,13 +7,12 @@ class UI {
 		this.shader = null;
 		this.currentScroll = [0, 0];
 
-		this.initShader();
-		this.initAnimationEvents();
+		this.initHeroShader();
 		this.initMetaballs();
 		this.initModal();
 	}
 
-	initShader() {
+	initHeroShader() {
 		const heroCanvas = document.querySelector('.hero canvas');
 			heroCanvas.style.width = '100%';
 			heroCanvas.width  = heroCanvas.offsetWidth;
@@ -40,24 +37,6 @@ class UI {
 
 		const shader = new GlslCanvas(metaballsCanvas);
 			shader.uniformTexture('u_tex0', 'img/shader/8x8-bayer.png');
-	}
-
-	initAnimationEvents() {
-		const myImgs = document.querySelectorAll('.animate');
-
-		const observer = new IntersectionObserver(entries => {
-		  entries.forEach(entry => {
-			if (entry.intersectionRatio > 0) {
-			  console.log('in the view', entry.intersectionRatio);
-			} else {
-			  console.log('out of view');
-			}
-		  });
-		});
-
-		myImgs.forEach(image => {
-		  observer.observe(image);
-		});
 	}
 
 	initModal() {

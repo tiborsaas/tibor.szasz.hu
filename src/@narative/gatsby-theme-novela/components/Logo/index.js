77d426c98eee391bf9cc5fbef940e68a50b7ee29
Tibor Szasz <tszasz@gmail.com>
2019-10-02 23:04:48 +0200

    content(post): first post

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+/**
+ * Paste in your SVG logo and return it from this component.
+ * Make sure you have a height set for your logo.
+ * It is recommended to keep the height within 25-35px.
+ */
+export default function Logo() {
+    return (
+        <svg width="90" height="94" xmlns="http://www.w3.org/2000/svg">
+            <g id="svg_14" transform="matrix(1.3333333,0,0,-1.3333333,0,347.17333) ">
+                <g id="svg_15" transform="translate(0,28) scale(0.08) ">
+                    <g id="svg_17" clip-path="url(#svg_18)">
+                        <path fill="#181210" fill-rule="nonzero" d="m40.700012,2753.790039l0,-257.300049l171.14502,0l0,-85.780029l-171.14502,0l0,-85.780029l256.718933,0l0,257.340088l-171.136963,0l0,85.73999l427.855957,0l0,-85.73999l-171.125977,0l0,-257.340088l256.714966,0l0,85.780029l-171.120972,0l0,85.780029l171.120972,0l0,257.300049l-599.026917,0" id="svg_20" />
+                        <path fill="#181210" fill-rule="nonzero" d="m40.700012,2153.380127l599.026917,0l0,85.773193l-599.026917,0l0,-85.773193z" id="svg_19" />
+                    </g>
+                </g>
+            </g>
+        </svg>)
+    ;
+}

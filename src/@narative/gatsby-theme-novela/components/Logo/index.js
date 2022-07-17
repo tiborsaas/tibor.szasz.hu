6e22194cdf431f4e0208cd6fdf717c0f04162eb8
Tibor Szasz <tszasz@gmail.com>
2022-07-17 16:18:14 +0200

    feat(ga): add analytics 4.0 code

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Helmet from 'react-helmet';
 
 /**
  * Paste in your SVG logo and return it from this component.
@@ -7,15 +8,29 @@ import React from "react";
  */
 export default function Logo() {
     return (
-        <svg width="90" height="94" xmlns="http://www.w3.org/2000/svg">
-            <g id="svg_14" transform="matrix(1.3333333,0,0,-1.3333333,0,347.17333) ">
-                <g id="svg_15" transform="translate(0,28) scale(0.08) ">
-                    <g id="svg_17" clipPath="url(#svg_18)">
-                        <path fill="#181210" fillRule="nonzero" d="m40.700012,2753.790039l0,-257.300049l171.14502,0l0,-85.780029l-171.14502,0l0,-85.780029l256.718933,0l0,257.340088l-171.136963,0l0,85.73999l427.855957,0l0,-85.73999l-171.125977,0l0,-257.340088l256.714966,0l0,85.780029l-171.120972,0l0,85.780029l171.120972,0l0,257.300049l-599.026917,0" id="svg_20" />
-                        <path fill="#181210" fillRule="nonzero" d="m40.700012,2153.380127l599.026917,0l0,85.773193l-599.026917,0l0,-85.773193z" id="svg_19" />
-                    </g>
-                </g>
-            </g>
-        </svg>)
+		<>
+			<Helmet>
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-9QCQ2RCGPE"></script>
+				<script>
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments)};
+						gtag('js', new Date());
+						gtag('config', 'G-9QCQ2RCGPE');
+					`}
+				</script>
+			</Helmet>
+			<svg width="90" height="94" xmlns="http://www.w3.org/2000/svg">
+				<g id="svg_14" transform="matrix(1.3333333,0,0,-1.3333333,0,347.17333) ">
+					<g id="svg_15" transform="translate(0,28) scale(0.08) ">
+						<g id="svg_17" clipPath="url(#svg_18)">
+							<path fill="#181210" fillRule="nonzero" d="m40.700012,2753.790039l0,-257.300049l171.14502,0l0,-85.780029l-171.14502,0l0,-85.780029l256.718933,0l0,257.340088l-171.136963,0l0,85.73999l427.855957,0l0,-85.73999l-171.125977,0l0,-257.340088l256.714966,0l0,85.780029l-171.120972,0l0,85.780029l171.120972,0l0,257.300049l-599.026917,0" id="svg_20" />
+							<path fill="#181210" fillRule="nonzero" d="m40.700012,2153.380127l599.026917,0l0,85.773193l-599.026917,0l0,-85.773193z" id="svg_19" />
+						</g>
+					</g>
+				</g>
+			</svg>
+		</>
+	)
     ;
 }

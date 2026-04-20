4259e1361fe07d4e4955d631c398da3c24a21f2b
Tibor Szasz <tszasz@gmail.com>
2026-04-20 13:33:58 +0200

    feat(next): add asset config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    loader: "custom",
     formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "instant-storage.s3.us-east-2.amazonaws.com",
+      },
+    ],
+  },
+  turbopack: {
+    rules: {
+      "*.glsl": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+    },
   },
   webpack: (config) => {
     config.module.rules.push({
